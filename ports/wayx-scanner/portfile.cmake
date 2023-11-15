60e0261641edf7f5d9fbe8ec496164c86f65873a
Jared Irwin <jrairwin@sympatico.ca>
2023-11-15 11:24:10 -0500

    [wayx-scanner] Update Port

--- ports/wayx-scanner/portfile.cmake
+++ ports/wayx-scanner/portfile.cmake
@@ -6,7 +6,7 @@ endif()
 vcpkg_from_git(
 	OUT_SOURCE_PATH SOURCE_PATH
 	URL https://git.link1j.me/link1j/wayx-scanner.git
-	REF cd80686014a3377cae464f99996d776ec0d3a155
+	REF b1b7648aba73fd3221c3d98894dec5d6e92ae48f
 	HEAD_REF master
 	PATCHES deps.patch cli11.patch
 )
