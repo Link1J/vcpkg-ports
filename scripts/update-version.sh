efd3b6725561d2dba61971eb4b8bcabc6fdda26f
Jared Irwin <jrairwin@sympatico.ca>
2023-12-11 18:08:27 -0500

    [wayx-scanner] Update Port

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -9,6 +9,6 @@ NAME="$1"
 shift
 
 git commit -a -m "[$NAME] Update Port"
-vcpkg --x-builtin-ports-root=./ports --x-builtin-registry-versions-dir=./versions x-add-version $NAME --overwrite-version
+vcpkg --x-builtin-ports-root=./ports --x-builtin-registry-versions-dir=./versions x-add-version $NAME
 git commit -a -m "[$NAME] Update Port"
 git push
