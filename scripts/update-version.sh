16e1c947e34644c873ca6b8c7485b4566c9bfc8e
Jared Irwin <jrairwin@sympatico.ca>
2024-04-07 12:04:44 -0400

    Fix update script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -9,9 +9,9 @@ NAME="$1"
 shift
 
 vcpkg format-manifest ports/$NAME/vcpkg.json
-git commit ports/$NAME/.
+git add ports/$NAME/.
 git commit -m "[$NAME] Update Port"
 vcpkg --x-builtin-ports-root=./ports --x-builtin-registry-versions-dir=./versions x-add-version $NAME
-git commit versions/.
+git add versions/.
 git commit -m "[$NAME] Update Port" --amend
 git push
